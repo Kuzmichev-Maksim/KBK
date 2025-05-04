c160480534382bffd2a30bb3093774bdae597843
Kuzmichev-Maksim <isip_m.v.kuzmichev@mpt.ru>
2025-05-05 02:26:16 +0300

    КБК

--- stats/views.py
+++ stats/views.py
@@ -17,7 +17,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
-from rest_framework.decorators import AllowAny
+from rest_framework.permissions import AllowAny
+
 
 logger = logging.getLogger(__name__)
 
