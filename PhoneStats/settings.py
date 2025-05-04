b30f9e8968480fdc2609e7a2e68c960ad4b15c87
Kuzmichev-Maksim <isip_m.v.kuzmichev@mpt.ru>
2025-05-04 22:48:28 +0300

    KBK

--- PhoneStats/settings.py
+++ PhoneStats/settings.py
@@ -122,6 +122,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'stats/static'),  # Добавьте этот путь
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGGING = {
